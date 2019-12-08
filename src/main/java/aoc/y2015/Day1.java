f59c634f0316b1d9f86d58d7082c4a6079ae9bb8
fdfzcq <fdfzcq@gmail.com>
2019-12-08 22:10:34 +0100

    Day1

---
+++ src/main/java/aoc/y2015/Day1.java
@@ -0,0 +1,27 @@
+package aoc.y2015;
+
+import java.io.IOException;
+
+import aoc.Input;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+        String input = Input.input();
+        int result = processInput(input);
+
+        System.out.println(result);
+    }
+
+    public static int processInput(String input) {
+        char[] charList = input.toCharArray();
+        int result = 0;
+        for(int i = 0; i < charList.length; i++) {
+            int value = charList[i] == '(' ? 1 : -1;
+            result += value;
+            if (result == -1) {
+                return i + 1;
+            }
+        };
+        return result;
+    }
+}
